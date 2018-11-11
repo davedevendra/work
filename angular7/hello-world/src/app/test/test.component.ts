e65371aa59ab00a5ee91e05e0074562c87ec9cb0
Devendra Dave <devendra@localhost.localdomain>
2018-11-11 15:05:07 +0530

    commit

--- angular7/hello-world/src/app/test/test.component.ts
+++ angular7/hello-world/src/app/test/test.component.ts
@@ -3,12 +3,38 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
-  styleUrls: ['./test.component.css']
-})
+  styles: [`app-test {
+      color: red;
+    }
+
+    .text-success {
+        color: green;
+    }
+
+
+
+    .text-special {
+        font-style: italic;
+    }`]
+
+  })
 export class TestComponent implements OnInit {
 
   public name: string = "Dave";
   public myId: string = "myTestId";
+  public isDisabled: boolean = false;
+
+  public isSuccess: string = "text-success";
+  public error: string = "error";
+  public hasError: boolean = true;
+  public isSpecial: boolean = true;
+
+  public messageClasses = {
+    "text-success": !this.hasError,
+    "text-danger": this.hasError,
+    "text-special": this.isSpecial
+  }
+
   
   constructor() { }
 
